f68cc815d822284ef897e65711f5a15fb662ca00
Schmarni <marnistromer@gmail.com>
2025-03-31 15:48:22 +0200

    refactor(pen): update pen element
    Signed-off-by: Schmarni <marnistromer@gmail.com>

--- src/elements/pen.rs
+++ src/elements/pen.rs
@@ -1,43 +1,47 @@
-use std::f32::consts::{FRAC_PI_2, PI};
+use std::f32::consts::FRAC_PI_2;
 
 use crate::{
 	custom::{ElementTrait, FnWrapper},
 	ValidState,
 };
+use derive_setters::Setters;
 use glam::{Quat, Vec3};
+use mint::{Quaternion, Vector3};
 use stardust_xr_fusion::{
 	drawable::{Line, LinePoint, Lines, LinesAspect},
 	fields::{CylinderShape, Field, FieldAspect, Shape},
 	input::{InputData, InputDataType, InputHandler},
 	node::NodeError,
 	spatial::{Spatial, SpatialAspect, Transform},
-	values::color::{color_space::LinearRgb, AlphaColor, Rgb},
+	values::color::{color_space::LinearRgb, rgba_linear, AlphaColor, Rgb},
 };
-use stardust_xr_molecules::input_action::{
-	InputQueue, InputQueueable as _, SimpleAction, SingleAction,
-};
+use stardust_xr_molecules::input_action::{InputQueue, InputQueueable as _, SingleAction};
 
 #[derive_where::derive_where(Debug)]
+#[derive(Setters)]
+#[setters(into, strip_option)]
 pub struct Pen<State: ValidState> {
-	/// only send move updates after the pen has moved a distance greater than this since the last
-	/// move update
-	pub move_resolution: f32,
 	pub length: f32,
 	pub thickness: f32,
 	pub grab_distance: f32,
 	pub color: AlphaColor<f32, Rgb<f32, LinearRgb>>,
-	pub child_anchor: PenChildAnchor,
-	pub should_interact: FnWrapper<dyn Fn(&InputData) -> bool + Send + Sync>,
-	pub on_interact_start: FnWrapper<dyn Fn(&mut State) + Send + Sync>,
+	pub pos: Vector3<f32>,
+	pub rot: Quaternion<f32>,
 	#[expect(clippy::type_complexity)]
-	pub on_interact: FnWrapper<dyn Fn(&mut State, Vec3, &InputData) + Send + Sync>,
-	pub on_interact_stop: FnWrapper<dyn Fn(&mut State) + Send + Sync>,
+	pub update:
+		FnWrapper<dyn Fn(&mut State, PenState, Vector3<f32>, Quaternion<f32>) + Send + Sync>,
+	pub drawing_value: FnWrapper<dyn Fn(&InputData) -> f32 + Send + Sync>,
+	pub should_draw: FnWrapper<dyn Fn(&InputData) -> bool + Send + Sync>,
 }
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum PenChildAnchor {
-	Tip,
-	Top,
+
+#[derive(Debug)]
+pub enum PenState {
+	Grabbed,
+	StartedDrawing(f32),
+	Drawing(f32),
+	StoppedDrawing,
 }
+
 impl<State: ValidState> ElementTrait<State> for Pen<State> {
 	type Inner = PenInner;
 
@@ -65,10 +69,7 @@ impl<State: ValidState> ElementTrait<State> for Pen<State> {
 
 		let child_root = Spatial::create(
 			&pen_root,
-			match self.child_anchor {
-				PenChildAnchor::Tip => Transform::from_rotation(Quat::from_rotation_x(PI)),
-				PenChildAnchor::Top => Transform::from_translation(Vec3::new(0., self.length, 0.)),
-			},
+			Transform::from_translation(Vec3::new(0., self.length, 0.)),
 			false,
 		)?;
 
@@ -77,10 +78,9 @@ impl<State: ValidState> ElementTrait<State> for Pen<State> {
 			pen_root,
 			queue,
 			grab_action: Default::default(),
-			interact_action: Default::default(),
 			visuals,
 			child_root,
-			last_update_position: Vec3::ZERO,
+			drawing: false,
 		})
 	}
 
@@ -95,12 +95,13 @@ impl<State: ValidState> ElementTrait<State> for Pen<State> {
 		if !inner.queue.handle_events() {
 			return;
 		}
-		let last_actor = inner.grab_action.actor().cloned();
 		inner.grab_action.update(
 			false,
 			&inner.queue,
 			// should this even be exposed?
-			|data| data.distance < self.grab_distance,
+			|data| {
+				data.distance < self.grab_distance
+			},
 			|data| {
 				data.datamap.with_data(|datamap| match &data.input {
 					// should we also expose these values?
@@ -110,9 +111,6 @@ impl<State: ValidState> ElementTrait<State> for Pen<State> {
 				})
 			},
 		);
-		inner
-			.interact_action
-			.update(&inner.queue, &self.should_interact.0);
 
 		if inner.grab_action.actor_started() {
 			let _ = inner.pen_root.set_zoneable(false);
@@ -120,45 +118,45 @@ impl<State: ValidState> ElementTrait<State> for Pen<State> {
 		if inner.grab_action.actor_stopped() {
 			let _ = inner.pen_root.set_zoneable(true);
 		}
-		if let Some(actor) = inner.grab_action.actor().or(last_actor.as_ref()) {
-			if inner.interact_action.stopped_acting().contains(actor) {
-				self.on_interact_stop.0(state)
-			}
-		}
 		let Some(actor) = inner.grab_action.actor() else {
+			if inner.drawing {
+				inner.drawing = false;
+				self.update.0(state, PenState::StoppedDrawing, self.pos, self.rot);
+			}
 			return;
 		};
-		if inner.interact_action.started_acting().contains(actor) {
-			self.on_interact_start.0(state)
-		}
-		let transform = match &actor.input {
-			InputDataType::Hand(h) => Transform::from_translation_rotation(
+		let (pos, rot) = match &actor.input {
+			InputDataType::Hand(h) => (
 				(Vec3::from(h.thumb.tip.position) + Vec3::from(h.index.tip.position)) * 0.5,
 				Quat::from(h.palm.rotation) * Quat::from_rotation_x(FRAC_PI_2),
 			),
-			InputDataType::Tip(t) => Transform::from_translation_rotation(
-				t.origin,
+			InputDataType::Tip(t) => (
+				t.origin.into(),
 				Quat::from(t.orientation) * Quat::from_rotation_x(FRAC_PI_2),
 			),
-			_ => Transform::none(),
+			_ => (Vec3::ZERO, Quat::IDENTITY),
 		};
+		let transform = Transform::from_translation_rotation(pos, rot);
 		let _ = inner
 			.pen_root
 			.set_relative_transform(inner.queue.handler(), transform);
-		if inner.interact_action.currently_acting().contains(actor) {
-			let point = match &actor.input {
-				InputDataType::Hand(h) => {
-					(Vec3::from(h.thumb.tip.position) + Vec3::from(h.index.tip.position)) * 0.5
-				}
+		let mut pen_state = PenState::Grabbed;
+		let drawing = self.should_draw.0(actor);
+		let started_drawing = drawing && !inner.drawing;
+		let stopped_drawing = inner.drawing && !drawing;
+		inner.drawing = drawing;
 
-				InputDataType::Tip(t) => Vec3::from(t.origin),
-				_ => unreachable!(),
-			};
-			if point.distance(inner.last_update_position) >= self.move_resolution {
-				inner.last_update_position = point;
-				self.on_interact.0(state, point, actor);
-			}
+		if inner.drawing && !started_drawing {
+			pen_state = PenState::Drawing(self.drawing_value.0(actor));
 		}
+		if started_drawing {
+			pen_state = PenState::StartedDrawing(self.drawing_value.0(actor));
+		}
+		if stopped_drawing {
+			pen_state = PenState::StoppedDrawing;
+		}
+
+		self.update.0(state, pen_state, pos.into(), rot.into());
 	}
 
 	fn spatial_aspect(&self, inner: &Self::Inner) -> stardust_xr_fusion::spatial::SpatialRef {
@@ -167,17 +165,39 @@ impl<State: ValidState> ElementTrait<State> for Pen<State> {
 }
 
 impl<State: ValidState> Pen<State> {
-	fn react_to_changes(&self, old_decl: &Self, inner: &mut PenInner) {
-		if self.child_anchor != old_decl.child_anchor || self.length != old_decl.length {
-			_ = inner
-				.child_root
-				.set_local_transform(match self.child_anchor {
-					PenChildAnchor::Tip => Transform::from_rotation(Quat::from_rotation_x(PI)),
-					PenChildAnchor::Top => {
-						Transform::from_translation(Vec3::new(0., self.length, 0.))
-					}
-				});
+	pub fn new(
+		pos: impl Into<Vector3<f32>>,
+		rot: impl Into<Quaternion<f32>>,
+		update: impl Fn(&mut State, PenState, Vector3<f32>, Quaternion<f32>) + Send + Sync + 'static,
+	) -> Self {
+		Pen {
+			length: 0.075,
+			thickness: 0.005,
+			grab_distance: 0.05,
+			color: rgba_linear!(1.0, 1.0, 1.0, 1.0),
+			pos: pos.into(),
+			rot: rot.into(),
+			update: FnWrapper(Box::new(update)),
+			drawing_value: FnWrapper(Box::new(|data| {
+				data.datamap.with_data(|datamap| match &data.input {
+					InputDataType::Hand(_) => datamap.idx("pinch_strength").as_f32(),
+					InputDataType::Tip(_) => datamap.idx("select").as_f32(),
+					_ => unimplemented!(),
+				})
+			})),
+			should_draw: FnWrapper(Box::new(|data| {
+				data.datamap.with_data(|datamap| match &data.input {
+					InputDataType::Hand(_) => datamap.idx("pinch_strength").as_f32() > 0.3,
+					InputDataType::Tip(_) => datamap.idx("select").as_f32() > 0.01,
+					_ => false,
+				})
+			})),
 		}
+	}
+}
+
+impl<State: ValidState> Pen<State> {
+	fn react_to_changes(&self, old_decl: &Self, inner: &mut PenInner) {
 		if self.thickness != old_decl.thickness || self.length != old_decl.length {
 			_ = inner.visuals.set_lines(&[self.get_lines()]);
 			_ = inner.field.set_shape(Shape::Cylinder(CylinderShape {
@@ -216,7 +236,6 @@ pub struct PenInner {
 	pen_root: Spatial,
 	queue: InputQueue,
 	grab_action: SingleAction,
-	interact_action: SimpleAction,
 	visuals: Lines,
-	last_update_position: Vec3,
+	drawing: bool,
 }
